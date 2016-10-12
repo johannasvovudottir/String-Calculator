37431491019cd339c286b2883bc068124dfdc39a
Kobblander <kobblander@gmail.com>
2016-10-12 16:09:22 +0000

    Got compiler to work, added first test and add function

---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,10 @@
+package is.ru.stringcalculator;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class CalculatorTest
+{
+	public void testEmptyString()
+	{
+		assertEquals(0, Calculator.add(""));
+	}
+}
