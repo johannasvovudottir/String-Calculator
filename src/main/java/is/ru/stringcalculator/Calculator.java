1a08a16107ef7391ca5807dc16fbeee8e483b324
Kobblander <kobblander@gmail.com>
2016-10-13 15:29:47 +0000

    test two numbers added

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,9 +6,22 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
+		else if(text.contains(",")){
+			return sum(splitNumbers(text));
+		}
 		else
 			return 1;
 	}
 
+	private static String[] splitNumbers(String numbers){
+	    return numbers.split(",");
+	}
 
+	private static int sum(String[] numbers){
+ 	    int total = 0;
+        for(String number : numbers){
+		    total += toInt(number);
+		}
+		return total;
+    }
 }
