bb3bcf6851e295a99d144b64afd73ade7f993d40
Kobblander <kobblander@gmail.com>
2016-10-13 15:34:39 +0000

    test multiple numbers added

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -13,15 +13,22 @@ public class Calculator {
 			return 1;
 	}
 
+	private static int toInt(String number){
+		return Integer.parseInt(number);
+	}
+
 	private static String[] splitNumbers(String numbers){
 	    return numbers.split(",");
 	}
-
-	private static int sum(String[] numbers){
+      
+    private static int sum(String[] numbers){
  	    int total = 0;
         for(String number : numbers){
 		    total += toInt(number);
 		}
 		return total;
     }
+
+
+
 }
